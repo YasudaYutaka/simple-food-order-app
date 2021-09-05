de62954ab0505b636844b0a75348281d54b60263
Rafael Yutaka Yasuda <62554340+YasudaYutaka@users.noreply.github.com>
2021-09-05 15:12:07 -0300

    useContext & hard coded removed

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,20 +1,46 @@
-import Modal from '../UI/Modal';
+import { useContext } from "react";
+import Modal from "../UI/Modal";
 import styles from "./Cart.module.css";
+import CartContext from "../../store/cart-context";
+import CartItem from "./CartItem";
 
 const Cart = (props) => {
+  const cartCtx = useContext(CartContext);
 
-  const cartItems = <ul className={styles['cart-items']}>{[{id: 'c1', name: 'Sushi', amount: 2, price: 12.99}].map(item => <li>{item.name}</li>)}</ul>;
+  const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
+  const hasItems = cartCtx.items.length > 0;
+
+  const cartItemRemoveHandler = (id) => {};
+
+  const cartItemAddHandler = (item) => {};
+
+  const cartItems = (
+    <ul className={styles["cart-items"]}>
+      {cartCtx.items.map((item) => (
+        <CartItem
+          key={item.id}
+          name={item.name}
+          amount={item.amount}
+          price={item.price}
+          onRemove={cartItemRemoveHandler.bind(null, item.id)}
+          onAdd={cartItemAddHandler.bind(null, item)}
+        />
+      ))}
+    </ul>
+  );
 
   return (
     <Modal onHideCart={props.onHideCart}>
       {cartItems}
       <div className={styles.total}>
-          <span>Total Amount</span>
-          <span>35.62</span>
+        <span>Total Amount</span>
+        <span>{totalAmount}</span>
       </div>
       <div className={styles.actions}>
-          <button className={styles['button--alt']} onClick={props.onHideCart}>Close</button>
-          <button className={styles.button}>Order</button>
+        <button className={styles["button--alt"]} onClick={props.onHideCart}>
+          Close
+        </button>
+        {hasItems && <button className={styles.button}>Order</button>}
       </div>
     </Modal>
   );
