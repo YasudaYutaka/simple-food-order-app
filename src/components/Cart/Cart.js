e775b4fad5d3eb7c6828ce84e1da977779a8c755
Rafael Yutaka Yasuda <62554340+YasudaYutaka@users.noreply.github.com>
2021-09-05 14:07:54 -0300

    onClick & onHideCart prop

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,9 +1,23 @@
-import styles from './Cart.module.css';
+import Modal from '../UI/Modal';
+import styles from "./Cart.module.css";
 
 const Cart = (props) => {
-    return (
 
-    );
-}
+  const cartItems = <ul className={styles['cart-items']}>{[{id: 'c1', name: 'Sushi', amount: 2, price: 12.99}].map(item => <li>{item.name}</li>)}</ul>;
+
+  return (
+    <Modal onHideCart={props.onHideCart}>
+      {cartItems}
+      <div className={styles.total}>
+          <span>Total Amount</span>
+          <span>35.62</span>
+      </div>
+      <div className={styles.actions}>
+          <button className={styles['button--alt']} onClick={props.onHideCart}>Close</button>
+          <button className={styles.button}>Order</button>
+      </div>
+    </Modal>
+  );
+};
 
 export default Cart;
