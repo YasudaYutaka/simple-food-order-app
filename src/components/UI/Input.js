a9697aad38ba76e20daec2af18743b907b0ed7d7
Rafael Yutaka Yasuda <62554340+YasudaYutaka@users.noreply.github.com>
2021-09-05 13:34:14 -0300

    Input

---
+++ src/components/UI/Input.js
@@ -0,0 +1,12 @@
+import styles from './Input.module.css';
+
+const Input = (props) => {
+    return (
+        <div className={styles.input}>
+            <label htmlFor={props.input.id}>{props.label}</label>
+            <input {...props.input} />
+        </div>
+    );
+}
+
+export default Input;
